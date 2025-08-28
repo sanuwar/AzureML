c5e2b1ec90427cd1d0eeecc361b868dc2a2f6657
Sanuwar <sanuwar.rashid@gmail.com>
2025-08-28 09:45:29 -0400

    adding Challenge 0: ipynb to py Challenge 1: create a ML Job

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,18 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    try:
+        import mlflow
+        mlflow.autolog()
+    except Exception:
+        pass  # continue if mlflow isn't installed
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +40,12 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure',
+            'TricepsThickness','SerumInsulin','BMI',
+            'DiabetesPedigree','Age']].values
+    y = df['Diabetic'].values
+    return train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
