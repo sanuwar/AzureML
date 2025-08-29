75c7368bc05eaaa891c142968e4c80b375137664
Sanuwar <sanuwar.rashid@gmail.com>
2025-08-28 22:13:22 -0400

    style: format with black/isort; fix lint

--- src/model/train.py
+++ src/model/train.py
@@ -5,10 +5,8 @@ import glob
 import os
 
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
 
 
 # define functions
@@ -16,6 +14,7 @@ def main(args):
     # TO DO: enable autologging
     try:
         import mlflow
+
         mlflow.autolog()
     except Exception:
         pass  # continue if mlflow isn't installed
@@ -41,15 +40,25 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure',
-            'TricepsThickness','SerumInsulin','BMI',
-            'DiabetesPedigree','Age']].values
-    y = df['Diabetic'].values
+    X = df[
+        [
+            "Pregnancies",
+            "PlasmaGlucose",
+            "DiastolicBloodPressure",
+            "TricepsThickness",
+            "SerumInsulin",
+            "BMI",
+            "DiabetesPedigree",
+            "Age",
+        ]
+    ].values
+    y = df["Diabetic"].values
     return train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
@@ -57,16 +66,15 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument("--training_data", dest="training_data", type=str)
+    parser.add_argument("--reg_rate", dest="reg_rate", type=float, default=0.01)
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
